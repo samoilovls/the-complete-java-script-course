037f2f6b57579b5cd35101251affaef79987061e
samoilovls <samoilovl2000@gmail.com>
2023-01-24 15:36:27 +0300

    Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -34,6 +34,7 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 // Function Declaration:
+
 // We can call them in a code before they are defined:
 const age1 = calcAge1(1991);
 
@@ -106,7 +107,6 @@ const yearsUntilRetirement1 = function (birthYear, firstName) {
 yearsUntilRetirement1(1991, 'Jonas');
 console.log(yearsUntilRetirement1(1991, 'Jonas'));
 // console.log(yearsUntilRetirement1(1950, 'Mike'));
-*/
 
 // Coding Challenge 1
 
@@ -131,4 +131,75 @@ function checkWinner (avgDolphins,avgKoalas) {
 }
 
 console.log(checkWinner(avgDolphins,avgKoalas));
-console.log(checkWinner(avgDolphins2,avgKoalas2));
+console.log(checkWinner(avgDolphins2,avgKoalas2)); 
+
+
+// Arrays:
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+// const years = new Array(1991, 1984, 2000, 2020);
+// console.log(years);
+
+console.log(friends[0]); // 0 based
+console.log(friends[2]);
+
+console.log(friends.length); // property, not 0 based
+console.log(friends[friends.length-1]);
+
+friends[2] = 'Jay'; // Array is not a primitive value , only primitive values are immutable
+// But we can not replace the entire Array: friends = ['Bob', 'Alice']
+console.log(friends);
+
+const firstName = 'Lev';
+const me = [firstName, 'Samoilov', 2023 - 2000, friends];
+console.log(me);
+
+// Exercise
+const calcAge = function (birthYear){
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length-1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length-1])];
+console.log(ages);
+*/
+
+
+// Basic Array methods:
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+const newLength = friends.push('Jay'); // add an element to the end of the array, returns length
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John'); // add an element to the beginning of the array
+console.log(friends);
+
+
+friends.pop(); // remove the last element of the array, returns the removed element
+const popped = friends.pop();
+console.log(friends);
+console.log(popped);
+
+friends.shift(); // remove the first element
+console.log(friends);
+
+console.log(friends.indexOf('Steven')); // return in which position a certain element is in the array
+
+console.log(friends.includes('Bob')); // return true if the element is in the array or false / doesn't do type coercion
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven');
+}
