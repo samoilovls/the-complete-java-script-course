faf8aaf0f89e6cf42edf3b5041c5a62bc5f88c46
samoilovls <samoilovl2000@gmail.com>
2023-06-11 16:10:56 +0300

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -19,13 +19,19 @@ let highscore = 0;
 // const highscore = [0];
 // document.querySelector('.number').textContent = number;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   // console.log(typeof guess, guess);
   if (!guess) {
-    document.querySelector('.message').textContent = 'Empty...';
+    // document.querySelector('.message').textContent = 'Empty...';
+    displayMessage('Empty...');
   } else if (guess === number) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    // document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number!');
     document.querySelector('.number').textContent = number;
 
     if (score > highscore) {
@@ -41,25 +47,41 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-  } else if (guess > number && guess > 0) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too big!';
-      score--; // score = score - 1
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lose!';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < number && guess > 0) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low!';
-      score--; // score = score - 1
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lose!';
-      document.querySelector('.score').textContent = 0;
-    }
+  } else if (guess !== number && score > 1) {
+    score--;
+    document.querySelector('.score').textContent = score;
+    // document.querySelector('.message').textContent = guess > number ? 'Too big!' : 'Too low!';
+    displayMessage(guess > number ? 'Too big!' : 'Too low!');
+    // if (guess > number) {
+    //   document.querySelector('.message').textContent = 'Too big!';
+    // } else {
+    //   document.querySelector('.message').textContent = 'Too low!';
+    // }
+  } else {
+    // document.querySelector('.message').textContent = 'You lose!';
+    displayMessage('You lose!');
+    document.querySelector('.score').textContent = 0;
+    document.querySelector('body').style.backgroundColor = '#cf2222';
   }
+  //  else if (guess > number && guess > 0) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'Too big!';
+  //     score--; // score = score - 1
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'You lose!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // } else if (guess < number && guess > 0) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'Too low!';
+  //     score--; // score = score - 1
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'You lose!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
 });
 
 // Coding Challenge #1
@@ -68,7 +90,8 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').textContent = '?';
   score = 20;
   document.querySelector('.score').textContent = score;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
