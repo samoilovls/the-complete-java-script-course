387ed07dd84cb756214327f93c4a0b079e97b685
samoilovls <samoilovl2000@gmail.com>
2023-11-14 18:56:01 +0300

    Destructuring Arrays

---
+++ 09-Data-Structures-Operators/starter/script.js
@@ -0,0 +1,81 @@
+'use strict';
+/*
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+*/
+// Destructuring Arrays:
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr; // destructuring assignment
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories; // skip the element
+console.log(main, secondary);
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+
+// Switch two variables using destructuring:
+// [main, secondary] equals the destructuring of this array: [secondary, main]:
+[main, secondary] = [secondary, main]; // reassigning the values of the two variables
+console.log(main, secondary, restaurant);
+
+// Receive two return values from a function:
+console.log(restaurant.order(2, 0));
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested Array:
+const nested = [2, 4, [5, 6]];
+const [first, , second] = nested;
+console.log(first, second);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Set default values:
+const [p = 1, q = 1, r = 1] = [8, 9]; // ??
+console.log(p, q, r);
