f75fd2b6839ad99417228d595dd92b0751c92528
samoilovls <samoilovl2000@gmail.com>
2023-11-24 16:22:02 +0300

    Coding Challenge #1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -318,7 +318,6 @@ if (restaurant.orderPizza) {
 
 restaurant.orderPizza && restaurant.orderPizza('bacon');
 
-*/
 
 // Nullish Coalescing operator
 // Nullish values: null and undefined (NOT 0 or '')
@@ -370,3 +369,86 @@ rest1.owner &&= 'ANONYMOUS';
 console.log(rest1);
 console.log(rest2);
 console.log(rest3);
+
+*/
+
+// Coding Challenge #1
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+// 2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+// 5.
+const {
+  odds: { team1: teamOne, x: drawW, team2: teamTwo },
+} = game;
+console.log(teamOne, drawW, teamTwo);
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+// 6.
+const printGoals = function (...goals) {
+  for (let i = 0; i < goals.length; i++) {
+    console.log(`${goals[i]} SCORE:${i + 1}`);
+  }
+};
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+const printGoalsScored = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+printGoalsScored('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoalsScored(...game.scored);
+// 7.
+// game.odds.team1 > game.odds.team2 || console.log('Team1 is more likely to win');
+team1 > team2 || console.log('Team1 is more likely to win');
+team1 < team2 && console.log('Team1 is more likely to win');
