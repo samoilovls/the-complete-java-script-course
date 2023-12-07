013d3a46d6ca64c8880a746d2837724d5e78fdb6
samoilovls <samoilovl2000@gmail.com>
2023-12-07 16:15:58 +0300

    117 Maps  Fundamentals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -713,7 +713,6 @@ for (const player of game.scored) {
   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
 }
 
-*/
 
 // SETS:
 // Data structure
@@ -763,3 +762,52 @@ console.log(staffUnique);
 // Conversion from a set to an array:
 const staffUniqueArray = [...staffUnique];
 console.log(staffUniqueArray);
+
+*/
+
+// MAPS:
+// Data structure to map values to keys. Data stored in key value pairs.
+// The keys can have any type.
+
+const rest = new Map();
+// fill up the map:
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+// .set returns the updated map allows us to chain the set method:
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+// Read data from a map:
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+// Example
+const time = 10;
+console.log(rest.get(time >= rest.get('open') && time < rest.get('close')));
+
+// Check a certain key:
+console.log(rest.has('categories'));
+// Delete elements:
+rest.delete(2);
+console.log(rest);
+// rest.clear() delete all the elements
+// Size:
+console.log(rest.size);
+
+// Objects and Arrays as a keys:
+rest.set([1, 2], 'Test #1');
+console.log(rest.get([1, 2])); // will not work because it is not the same object in the heap
+// In order to male it work:
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest.get(arr));
+
+// DOM elements (special type of object):
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
