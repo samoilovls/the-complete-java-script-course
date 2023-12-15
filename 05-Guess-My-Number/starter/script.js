56735c0598decc2364f4a3eadb4c7443b6b6e127
samoilovls <samoilovl2000@gmail.com>
2023-12-15 13:38:52 +0300

    Get the data from the input field

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,7 +9,7 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 18; // .value with input field
+document.querySelector('.guess').value = 18; // .value with input field to get the data from there
 
 */
 
