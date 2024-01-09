6d05cf805b48c59f185cf908cf9c7940fbb03734
samoilovls <samoilovl2000@gmail.com>
2024-01-09 18:58:20 +0300

    How primitive types and reference types work in the context of functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 
 // Default Parameters
 // ES6 function (parameter = any expression and here we can use the values of the other parameters that we set BEFORE it)
@@ -29,3 +30,36 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 // Skip arguments that we want to leave as default when we called a function:
 createBooking('LH123', undefined, 1000);
+
+*/
+
+// How passing arguments works / How primitive types and reference types work in the context of functions
+// JS does not have passing by reference, only passing by value.
+// For objects, we do in fact pass IN a reference: the memory address of the object, however that reference itself is still a value. It's simply a value that contains a memory address. We pass a reference to the function, but we do not pass BY reference.
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  // You SHOULD NOT change the parameters of a function:
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+    console.log(flightNum);
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+newPassport(jonas);
+checkIn(flight, jonas);
