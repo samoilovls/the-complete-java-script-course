b25dbc617d99672b027ba24ff1f341678c5ddb11
samoilovls <samoilovl2000@gmail.com>
2024-02-23 10:58:00 +0300

    Numeric Separators

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -385,3 +385,21 @@ labelBalance.addEventListener('click', function () {
 });
 
 */
+
+// Numeric Separators
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+// some restrictions:
+const PI = 3.1415; // _3_._14__15_ not allowed
+// converting strings with underscores to numbers does not work
+console.log(Number('230_000')); // NaN
+console.log(parseInt('230_000')); // only 230
