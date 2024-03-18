fbea6294a2d0a0035c97450d0fc382644755e414
samoilovls <samoilovl2000@gmail.com>
2024-03-18 17:53:18 +0300

    Coding Challenge #3

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -329,3 +329,44 @@ mike.calcAge();
 
 // Fix type
 Student.prototype.constructor = Student;
+
+// Coding Challenge #3
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+// Polymorphism
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+tesla.brake();
+tesla.accelerate();
+console.log(tesla);
