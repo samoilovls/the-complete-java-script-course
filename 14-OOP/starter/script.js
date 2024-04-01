da10a4bee2d7617aae48b2f24690b803d3529314
samoilovls <samoilovl2000@gmail.com>
2024-04-01 12:53:17 +0300

    Class Example

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -423,7 +423,6 @@ console.log(peter);
 peter.introduce();
 peter.calcAge();
 
-*/
 
 // Inheritance Between Classes: Object.create
 
@@ -455,3 +454,46 @@ jay.init('Jay', 2012, 4);
 jay.introduce();
 jay.calcAge();
 console.log(jay);
+
+*/
+
+// Class Example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    // we can execute any code in this constructor:
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+
+console.log(acc1);
