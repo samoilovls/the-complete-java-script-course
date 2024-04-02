e513df2361742c40ea4122f64a2ffdf0b373a46b
samoilovls <samoilovl2000@gmail.com>
2024-04-02 17:30:42 +0300

    Geolocation API

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,20 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Geolocation API
+// this function takes as an input two callback functions: the first one will be called on success, whenever the browser successfully got the coordinates, and the second is the error.
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    // console.log(position);
+    // const latitude = position.coords.latitude;
+    // const longitude = position.coords.longitude;
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.ru/maps/@${latitude},${longitude}`);
+    console.log(latitude, longitude);
+  },
+  function () {
+    alert('Could not get your position');
+  }
+);
