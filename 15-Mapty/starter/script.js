89a4552d014ff03df2afb713d5e9a79963f1da5e
samoilovls <samoilovl2000@gmail.com>
2024-04-03 14:23:49 +0300

    Leaflet Library

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -13,17 +13,30 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 // Geolocation API
 // this function takes as an input two callback functions: the first one will be called on success, whenever the browser successfully got the coordinates, and the second is the error.
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    // console.log(position);
-    // const latitude = position.coords.latitude;
-    // const longitude = position.coords.longitude;
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
-    console.log(`https://www.google.ru/maps/@${latitude},${longitude}`);
-    console.log(latitude, longitude);
-  },
-  function () {
-    alert('Could not get your position');
-  }
-);
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // console.log(position);
+      // const latitude = position.coords.latitude;
+      // const longitude = position.coords.longitude;
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.ru/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
+
+// Any variable that is global in any script will be available to all the other scripts as long as they appear after that script:
+// console.log(firstName);
