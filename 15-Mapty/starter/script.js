ceed33b97ff222c6ab4993a32638ed4f21c2f3e9
samoilovls <samoilovl2000@gmail.com>
2024-04-03 17:48:43 +0300

    Displaying a Map Marker

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -25,12 +25,34 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude];
 
+      // Leaflet Library:
       const map = L.map('map').setView(coords, 13);
+      // console.log(map);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
+
+      // leaflet event listener
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position');
