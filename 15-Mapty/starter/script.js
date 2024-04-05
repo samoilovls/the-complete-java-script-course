2fd9790bdcec8f56df51bb6f593fe388837508fa
samoilovls <samoilovl2000@gmail.com>
2024-04-05 17:07:35 +0300

    Project Architecture

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,6 +11,129 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class App {
+  workouts = [];
+  #map;
+  #mapEvent;
+
+  clearInputs = function () {
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    inputDistance.blur();
+    inputDuration.blur();
+    inputCadence.blur();
+    inputElevation.blur();
+  };
+
+  constructor() {
+    this.#getPosition();
+
+    form.addEventListener('submit', this.#newWorkout.bind(this));
+
+    inputType.addEventListener('change', this.#toggleInput.bind(this));
+  }
+
+  #getPosition() {
+    console.log(this);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this.#loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
+      );
+    }
+  }
+
+  #loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    // Leaflet Library:
+
+    this.#map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // leaflet event listener
+    this.#map.on('click', this.#showForm.bind(this));
+  }
+
+  #showForm(mapE) {
+    // click on map
+
+    this.#mapEvent = mapE;
+
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  #toggleInput() {
+    // change input
+
+    inputElevation.parentElement.classList.toggle('form__row--hidden');
+    inputCadence.parentElement.classList.toggle('form__row--hidden');
+  }
+
+  #newWorkout(e) {
+    // submit form
+    // create new child objects
+    // stored in workouts array
+
+    e.preventDefault();
+    // Display marker:
+
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+
+    this.clearInputs();
+  }
+
+  // _someMethod() {}
+}
+
+const app = new App();
+
+// console.log(app.workouts);
+// console.log(app.__proto__);
+// console.log(App.prototype);
+
+class Workout {
+  constructor(distance, duration, coords) {}
+}
+
+class Running extends Workout {
+  constructor(cadence, pace) {
+    super();
+  }
+}
+
+class Cycling extends Workout {
+  constructor(elevation, speed) {}
+}
+
+/*
 // Geolocation API
 // this function takes as an input two callback functions: the first one will be called on success, whenever the browser successfully got the coordinates, and the second is the error.
 
@@ -89,3 +212,4 @@ inputType.addEventListener('change', function () {
 
 // Any variable that is global in any script will be available to all the other scripts as long as they appear after that script:
 // console.log(firstName);
+*/
